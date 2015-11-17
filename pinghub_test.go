fe0223aebdc63bb06f47d62f80f9d4f3429df48f
Andy Skelton <skeltoac@gmail.com>
2015-11-17 10:10:26 -0600

    Test HTML template includes URL Path.

--- pinghub_test.go
+++ pinghub_test.go
@@ -16,9 +16,19 @@ func TestMain(m *testing.M) {
 func TestApp(t *testing.T) {
 	s := mockApp()
 	defer s.Close()
+
+	t.Log("Test: GET / serves HTML")
 	resp := get(t, s, "/")
-	body := getBody(t, resp)
+	body := responseBody(t, resp)
 	_, err := regexp.Match("<html>", body)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log("Test: GET /somestring contains /somestring")
+	resp = get(t, s, "/somestring")
+	body = responseBody(t, resp)
+	_, err = regexp.Match("/somestring", body)
 	if err != nil {
 		t.Error(err)
 	}
@@ -31,15 +41,15 @@ func mockApp() *httptest.Server {
 func get(t *testing.T, s *httptest.Server, path string) *http.Response {
 	resp, err := http.Get(s.URL + path)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	return resp
 }
 
-func getBody(t *testing.T, r *http.Response) []byte {
+func responseBody(t *testing.T, r *http.Response) []byte {
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
 	return body
 }
