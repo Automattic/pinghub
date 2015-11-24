ea1de0ebcb08c3401ca77a95f3ba948d8f05ac6d
Andy Skelton <skeltoac@gmail.com>
2015-11-24 19:29:41 +0000

    Use stoppable http server so signals can trigger shutdown.

--- main.go
+++ main.go
@@ -2,20 +2,37 @@ package main
 
 import (
 	"flag"
+	"github.com/facebookgo/httpdown"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
+	"time"
 )
 
-var addr = flag.String("addr", ":8081", "http service address")
 var origin = flag.String("origin", "", "websocket server checks Origin headers against this scheme://host[:port]")
 
 func main() {
+	// Prepare the stoppable HTTP server
+	handler := newHandler(*origin)
+	http.Handle("/", handler)
+	server := &http.Server{
+		Addr: "127.0.0.1:8081",
+		Handler: handler,
+	}
+	hd := &httpdown.HTTP{
+		StopTimeout: 10 * time.Second,
+		KillTimeout: 1 * time.Second,
+	}
+
+	// Apply command line arguments
+	flag.StringVar(&server.Addr, "addr", server.Addr, "http service address")
+	flag.DurationVar(&hd.StopTimeout, "stop-timeout", hd.StopTimeout, "stop timeout")
+	flag.DurationVar(&hd.KillTimeout, "kill-timeout", hd.KillTimeout, "kill timeout")
 	flag.Parse()
 
-	// Start the HTTP server
-	http.Handle("/", newHandler(*origin))
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	// Start the server
+	if err := httpdown.ListenAndServe(server, hd); err != nil {
+		panic(err)
+	}
 }
 
 func newHandler(origin string) http.Handler {
