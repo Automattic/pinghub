5cb04bca6f1cdf090febf494fabb508390a070bb
Andy Skelton <skeltoac@gmail.com>
2016-03-30 18:08:29 +0000

    Ping clients every 27 seconds

--- conn.go
+++ conn.go
@@ -2,6 +2,21 @@ package main
 
 import (
 	"github.com/gorilla/websocket"
+	"time"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 30 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
 )
 
 type connection struct {
@@ -37,6 +52,10 @@ func (c *connection) run() {
 }
 
 func (c *connection) reader() {
+	c.ws.SetReadLimit(maxMessageSize)
+	c.ws.SetReadDeadline(time.Now().Add(pongWait))
+	c.ws.SetPongHandler(func(s string) error { c.ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+
 	for {
 		_, message, err := c.ws.ReadMessage()
 		if err != nil {
@@ -52,12 +71,31 @@ func (c *connection) reader() {
 }
 
 func (c *connection) writer() {
-	for message := range c.send {
-		err := c.ws.WriteMessage(websocket.TextMessage, message)
-		if err != nil {
-			break
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.ws.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.write(websocket.CloseMessage, []byte{})
+				return
+			}
+			if err := c.write(websocket.TextMessage, message); err != nil {
+				return
+			}
+			mark("sends", 1)
+		case <-ticker.C:
+			if err := c.write(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
 		}
-		mark("sends", 1)
 	}
-	c.ws.Close()
+}
+
+func (c *connection) write(mt int, payload []byte) error {
+	c.ws.SetWriteDeadline(time.Now().Add(writeWait))
+	return c.ws.WriteMessage(mt, payload)
 }
