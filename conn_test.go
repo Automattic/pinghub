4a0993abee24b0f2230add5e5517b4ca38c9482c
Lauren Mermel <lauren@violetgrey.com>
2016-04-09 06:53:42 -0700

    Tests: clean up conn_test replace var w/ struct field

--- conn_test.go
+++ conn_test.go
@@ -6,12 +6,9 @@ import (
 	"time"
 )
 
-var testConnectionMessage []byte
-var errReadMsg error
-
 func TestConnWriterErr(t *testing.T) {
 	conn := newTestConnectionWS()
-	errReadMsg = errors.New("Message Read Error")
+	conn.w = mockWsInteractor{err: errors.New("Message Read Error")}
 
 	conn.reader()
 
@@ -22,13 +19,11 @@ func TestConnWriterErr(t *testing.T) {
 	if len(conn.channel.queue) != 0 {
 		t.Fatal("Expectation: conn channel length should be > 0, Received:", len(conn.channel.queue))
 	}
-	// reset errReadMsg to nil
-	errReadMsg = nil
 }
 
 func TestConnWriterMessage(t *testing.T) {
 	conn := newTestConnectionWS()
-	testConnectionMessage = []byte("banana")
+	conn.w = mockWsInteractor{msg: []byte("banana")}
 
 	defer func() {
 		if r := recover(); r != nil {
@@ -44,7 +39,7 @@ func TestConnWriterMessage(t *testing.T) {
 
 func TestConnWriterNilMessage(t *testing.T) {
 	conn := newTestConnectionWS()
-	testConnectionMessage = []byte("")
+	conn.w = mockWsInteractor{msg: []byte("")}
 
 	if len(conn.send) != 0 {
 		t.Fatal("Expectation: 0, Received:", len(conn.send))
@@ -64,13 +59,15 @@ func TestConnWriterNilMessage(t *testing.T) {
 
 func newTestConnectionWS() *connection {
 	conn := newTestConnection()
-	conn.w = mockWsInteractor{}
 	conn.channel = &channel{queue: make(queue, 16), path: "/monkey"}
 	conn.send = make(chan []byte, 256)
 	return conn
 }
 
-type mockWsInteractor struct{}
+type mockWsInteractor struct {
+	msg []byte
+	err error
+}
 
 func (mq mockWsInteractor) wsSetReadLimit() {}
 
@@ -81,5 +78,5 @@ func (mq mockWsInteractor) wsSetPongHandler() {}
 func (mq mockWsInteractor) wsClose() {}
 
 func (mq mockWsInteractor) wsReadMessage() (messageType int, p []byte, err error) {
-	return messageType, testConnectionMessage, errReadMsg
+	return messageType, mq.msg, mq.err
 }
