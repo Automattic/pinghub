170718eeb774f97cb5da59c395eb8a81d4566c10
Andrija Vučinić <andrija.vucinic@gmail.com>
2026-03-09 11:32:52 +0100

    Allow clients to send messages to the websocket (#12)
    Required for collaborative editing
    
    This reverts commit df81311e7a05394e98fc2081c7ecd18fef400e0d.
    This reverts commit 7e13d4edd8b4a00abfe0e6f2f56fc819b7bba0d6.

--- conn.go
+++ conn.go
@@ -66,9 +66,7 @@ func (c *connection) reader() {
 			c.send <- []byte{}
 			continue
 		}
-		// Ignore messages from websocket clients until configuration can enable this per path/pattern.
-		// We don't want to open an amplification attack surface with the introduction of global, public channels.
-		//c.channel.queue <- command{cmd: PUBLISH, path: c.path, text: message}
+		c.channel.queue <- command{cmd: PUBLISH, path: c.path, text: message}
 		mark("websocketmsgs", 1)
 	}
 	c.ws.Close()
